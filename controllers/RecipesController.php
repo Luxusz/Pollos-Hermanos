b1e0648f2cd1c327f97b4d6f59e3641b944b46b9
Francisco <fhrancisco.rios@gmail.com>
2017-06-22 09:05:16 -0400

    Proando

--- controllers/RecipesController.php
+++ controllers/RecipesController.php
@@ -6,29 +6,40 @@ use Yii;
 use app\models\Recipes;
 use app\models\RecipesSearch;
 use app\models\Users;
+use app\models\Admixtures;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
-use yii\filters\VerbFilter;
 use yii\helpers\ArrayHelper;
-
+use yii\filters\AccessControl;
+use yii\widgets\ActiveForm;
+use yii\web\Response;
 /**
  * RecipesController implements the CRUD actions for Recipes model.
  */
 class RecipesController extends Controller
 {
-    /**
+    /**PROBANDO
      * @inheritdoc
      */
     public function behaviors()
     {
-        return [
-            'verbs' => [
-                'class' => VerbFilter::className(),
-                'actions' => [
-                    'delete' => ['POST'],
+        return [
+        'access' => [
+            'class' => AccessControl::className(),
+            'rules' => [                
+                [
+                    'allow' => true,
+                    'actions' => ['index','create','view','viewnot','update','misrecetas','recetas','createdynamic'],
+                    'roles' => ['admin'],
+                ],
+                [
+                    'allow' => true,
+                    'actions' => ['index','create','view','viewnot','misrecetas','recetas','createdynamic'],
+                    'roles' => ['usuario'],
                 ],
             ],
-        ];
+        ],
+    ];
     }
 
     /**
@@ -38,11 +49,39 @@ class RecipesController extends Controller
     public function actionIndex()
     {
         $searchModel = new RecipesSearch();
-        $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
+        //$searchModel->users_id = Yii::$app->user->identity->id;
+        $dataProvider = $searchModel->searchXY(Yii::$app->request->queryParams);
         
        
 
         return $this->render('index', [
+            'searchModel' => $searchModel,
+            'dataProvider' => $dataProvider,
+        ]);
+    }
+    
+    public function actionMisrecetas()
+    {
+        $searchModel = new RecipesSearch();
+        $searchModel->users_id = Yii::$app->user->identity->id;
+        $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
+        
+       
+
+        return $this->render('misrecetas', [
+            'searchModel' => $searchModel,
+            'dataProvider' => $dataProvider,
+        ]);
+    }
+    
+    public function actionRecetas()
+    {
+        $searchModel = new RecipesSearch();
+        $dataProvider = $searchModel->searchX(Yii::$app->request->queryParams);
+        
+       
+
+        return $this->render('recetas', [
             'searchModel' => $searchModel,
             'dataProvider' => $dataProvider,
         ]);
@@ -56,6 +95,14 @@ class RecipesController extends Controller
     public function actionView($id)
     {
         return $this->render('view', [
+            'model' => $this->findModel($id),
+        ]);
+    }
+    
+    
+    public function actionViewnot($id)
+    {
+        return $this->render('viewnot', [
             'model' => $this->findModel($id),
         ]);
     }
@@ -82,6 +129,47 @@ class RecipesController extends Controller
             ]);
         }
     }
+    
+    
+    public function actionCreatedynamic()
+    {
+        $model = new Recipes();
+        if ($model->load(Yii::$app->request->post()) && Yii::$app->request->isAjax)
+        {
+            Yii::$app->response->format = Response::FORMAT_JSON;
+            return ActiveForm::validate($model);
+        }
+        $model->users_id = Yii::$app->user->identity->id;                
+        $request = Yii::$app->request;
+        if ($model->load($request->post())) {
+            $model->save();
+            $ingredientes = $request->post('admixtures');
+            $unidades = $request->post('unities');
+            $cantidades = $request->post('quantities');
+            foreach($ingredientes as $i=>$productoId){
+                $modelVP = new Admixtures();
+                $unidad = $unidades[$i];
+                $cantidad = $cantidades[$i];
+                $modelVP->products_id = $productoId;
+                $modelVP->unity = $unidad;
+                $modelVP->quantity = $cantidad;
+                $modelVP->recipes_id = $model->id;
+                if($modelVP->validate()){
+                    $modelVP->save();
+                }
+                else{
+                    print_r($model->errors);
+                }
+            }
+            
+            return $this->redirect(['view', 'id' => $model->id]);
+        } else {
+            return $this->render('createdynamic', [
+                'model' => $model,
+            ]);
+        }
+        
+    }
 
     /**
      * Updates an existing Recipes model.
@@ -92,12 +180,33 @@ class RecipesController extends Controller
     public function actionUpdate($id)
     {
         $model = $this->findModel($id);
-
+        $respaldo = $id;
         if ($model->load(Yii::$app->request->post()) && $model->save()) {
+            $request = Yii::$app->request;
+            $ingredientes = $request->post('admixtures');
+            $unidades = $request->post('unities');
+            $cantidades = $request->post('quantities');
+            $vpAnteriores = Admixtures::deleteAll(['recipes_id'=>$id]);
+            foreach($ingredientes as $i=>$productoId){
+                $modelVP = new Admixtures();
+                $unidad = $unidades[$i];
+                $cantidad = $cantidades[$i];
+                $modelVP->products_id = $productoId;
+                $modelVP->unity = $unidad;
+                $modelVP->quantity = $cantidad;
+                $modelVP->recipes_id = $model->id;
+                if($modelVP->validate()){
+                    $modelVP->save();
+                }
+                else{
+                    print_r($model->errors);
+                }
+            }
             return $this->redirect(['view', 'id' => $model->id]);
         } else {
             return $this->render('update', [
                 'model' => $model,
+                'respaldo' => $respaldo,
             ]);
         }
     }
@@ -131,3 +240,4 @@ class RecipesController extends Controller
         }
     }
 }
+
